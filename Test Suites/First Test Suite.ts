c5fd5217639cf6c4c5d057b7e81945e5c245ab5b
naufalhaekall <naufalhaekall@localhost>
2024-05-13 15:20:37 +0700

    First Commit

---
+++ Test Suites/First Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>First Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3bf1112d-0b55-4951-a45a-6d8b6e1ba25e</testSuiteGuid>
+</TestSuiteEntity>
